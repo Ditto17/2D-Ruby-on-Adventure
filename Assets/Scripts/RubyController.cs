3525486150bf8a9972887f2412d8dce53f68e9f7
Maverick <dittobitz17@gmail.com>
2020-04-24 00:25:06 +0530

    Animation added
    Enemy And Ruby Animation added

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,19 +7,19 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-    
+    public float timeInvincible = 2.0f;
     public int health { get { return currentHealth; }}
     int currentHealth;
-    
-    public float timeInvincible = 2.0f;
     bool isInvincible;
     float invincibleTimer;
-
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
     Rigidbody2D rigidbody2d;
  
     // Start is called before the first frame update
     void Start()
-    {
+    {   
+        animator = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
 
         currentHealth = maxHealth;
@@ -30,10 +30,22 @@ public class RubyController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-
+                
+        Vector2 move = new Vector2(horizontal, vertical);
+        
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+        
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
+        
+        position = position + move * speed * Time.deltaTime;
         
         rigidbody2d.MovePosition(position);
 
